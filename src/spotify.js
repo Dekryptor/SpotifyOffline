f166b14e485da2d6f62ade7a7bb38773b0da2a9e
John Naylor <jonaylor89@gmail.com>
2019-05-11 01:28:32 -0400

    Fill in spotify methods

--- src/spotify.js
+++ src/spotify.js
@@ -1,7 +1,50 @@
 
+const request = require('request');
+
+const spotify_url = 'https://api.spotify.com/v1';
+
 /**
  *  GET https://api.spotify.com/v1/users/{user_id}/playlists
  */
 function getUserPlaylists(access_token, user_id) {
-    return [];
+    console.log("getting user playlists");
+
+    const options = {
+        url: spotify_url + '/users/' + user_id + '/playlists',
+        headers: {
+            'Authorization': 'Bearer ' + access_token
+        }
+    };
+
+    return request.get(options, (err, res, body) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+
+        console.log('body: ', body);
+    });
+}
+
+/**
+  * GET https://api.spotify.com/v1/playlists/{playlist_id}/tracks
+  */
+function getPlaylistTracks(access_token, playlist_id) {
+    console.log('getting playlist tracks');
+
+    const options = {
+        url: spotify_url + '/playlists/' + playlist_id + '/tracks',
+        headers: {
+            'Authorization': 'Bearer ' + access_token
+        }
+    };
+
+    return request.get(options, (err, res, body) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+
+        console.log('body: ', body)
+    });
 }
--- src/youtube.js
+++ src/youtube.js
@@ -1,3 +1,12 @@
 
-const ytdl = require('youtube-dl');
+const ydl = require('youtube-dl');
 
+module.exports.downloadSong = function(title) {
+   ydl.exec('ytsearch1:' + title, ['-o', title + '.mp3', '-x', '--audio-format', 'mp3'], {}, (err, output) => {
+      if (err) {
+         console.error(err);
+         return;
+      }
+      console.log("OUTPUT: " + output.join('\n'));
+   });
+}
